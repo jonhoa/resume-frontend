e92fa25fc40cce7321e5a91c5b4484ac6faabecb
Jonathan <jonitoadriano@gmail.com>
2023-03-01 23:47:23 -0600

    minor change

--- src/Content.jsx
+++ src/Content.jsx
@@ -11,6 +11,7 @@ export function Content() {
       <p><a href=''>Student #4 Resume</a></p>
       <p><a href=''>Student #5 Resume</a></p>
       <p><a href=''>Student #6 Resume</a></p>
+      <p><a href=''>Student #7 Resume</a></p>
       <ProductsNew />
       <ProductsIndex />
     </div>
