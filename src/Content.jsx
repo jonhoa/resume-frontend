919705d6c8b764cb15bd8269dc963292e1593fc9
Jonhoa <jonitoadriano@gmail.com>
2023-05-24 23:40:16 -0500

    Axios fetch button

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,20 @@
 import { ProductsNew } from './ProductsNew';
 import { ProductsIndex } from './ProductsIndex';
+import axios from 'axios';
 
 export function Content() {
+  function axiosFetch() {
+    console.log('Clicking!');
+    axios.get('http://localhost:3000/people/all').then(response =>{
+      console.log(response.data);
+      console.log('Clicking!')
+    })
+
+  }
   return (
     <div class="container">
       <h1>Resume Builder</h1>
+      <button onClick= {axiosFetch}>Fetch Axios Request!</button>
       <p><a href=''>Student #1 Resume</a></p>
       <p><a href=''>Student #2 Resume</a></p>
       <p><a href=''>Student #3 Resume</a></p>
