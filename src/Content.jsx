540782f7dda22285a336be044fec08a0a899d919
Jonhoa <jonitoadriano@gmail.com>
2023-06-10 18:14:54 -0500

    conditional

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,5 +1,6 @@
 import { ProductsNew } from './ProductsNew';
 import { ProductsIndex } from './ProductsIndex';
+import { useEffect } from 'react';
 import axios from 'axios';
 
 export function Content() {
@@ -7,14 +8,22 @@ export function Content() {
   const fakeData = [
     { name: "Paulo", age: "26"}, {name: "Igor", age: "55"}
   ];
-  
+  let isClicked = false
+
   function axiosFetch() {
     axios.get('http://localhost:3000/people/all').then(response =>{
       console.log(response.data);
       resumeData = response.data;
       console.log(resumeData);
       console.log('Clicking!')
+      isClicked = true
     })
+    console.log(fakeData);
+
+    if(isClicked){
+      <View></View>
+    } 
+    
   }
   return (
     <div class="container">
@@ -28,6 +37,7 @@ export function Content() {
       <p><a href=''>Student #6 Resume</a></p>
       <p><a href=''>Student #7 Resume</a></p>
       <p><a href=''>Student #8 Resume</a></p>
+
       {fakeData.map((resData => (
         <div>
         <p>{resData.name}</p>
